a1183cc37629d08137434e39abe213d627056bb7
gopinettem <gopinath.nettem@gmail.com>
2025-08-26 16:54:51 -0500

    Testing

--- src/main/java/com/gopi/EventDrivenArch/EmailEventProducer.java
+++ src/main/java/com/gopi/EventDrivenArch/EmailEventProducer.java
@@ -14,8 +14,8 @@ public class EmailEventProducer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @Autowired
-    private Users users;
+//    @Autowired
+//    private Users users;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
--- src/main/java/com/gopi/EventDrivenArch/UserService.java
+++ src/main/java/com/gopi/EventDrivenArch/UserService.java
@@ -9,17 +9,19 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
 	private EmailEventProducer emailEventProducer;
+
 	public Users CreateUser(String userName,String password, String firstName, String emailID)
 	{
-		Users user=new Users();
-		user.setUsername(userName);
-		user.setPassword(password);
-		user.setFirstName(firstName);
-		user.setEmailID(emailID);
+		Users users=new Users();
+		users.setUsername(userName);
+		users.setPassword(password);
+		users.setFirstName(firstName);
+		users.setEmailID(emailID);
 		
 //		return userRepository.save(user);
-		Users savedUser = userRepository.save(user);
+		Users savedUser = userRepository.save(users);
 		System.out.println("✅ User saved to database: " + savedUser.getUsername());
 
 		// Send email event to Kafka
