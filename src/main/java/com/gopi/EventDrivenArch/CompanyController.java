457d48311aa9db4e83e8e4fdac42733013d29668
gopinettem <gopinath.nettem@gmail.com>
2025-09-09 13:34:54 -0500

    Testing

--- src/main/java/com/gopi/EventDrivenArch/CompanyController.java
+++ src/main/java/com/gopi/EventDrivenArch/CompanyController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/company")
 //@CrossOrigin(origins = "*") // Allow CORS for frontend form
-@CrossOrigin(origins = {"http://18.222.145.215"/*, "https://yourdomain.com"*/})
+@CrossOrigin(origins = {"http://52.14.2.86"/*, "https://yourdomain.com"*/})
 public class CompanyController {
 
 	@Autowired
